7c9f42743014c6a834c1d68bfe1a35ebc5109182
fabi0215/ <fabianeranzani@gmail.com>
2024-06-29 03:28:53 -0300

    herois

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console .log ("olá pessoal");
+let nomes_herois = [" superman" , " batman " ,  " flash " ];
+console .log ("olá herois " + nomes_herois);
+
+let forca =  [" de 0 - 100"     = 95 , 75 , 60]
+let agilidade = [" de 0 - 100"  = 100 , 95 , 60]
+let coragem = [" de 0 - 100"    = 100 , 100 , 90]
+
+let habilidades_dos_herois = [
+[" superman " , (" forca " +  95 ) , (" agilidade " +  95 ) , (" coragem " +  95 ) ],
+[" batman" , 60 , 60 , 100 ],
+[" flash " , 75 , 100 , 90 ],
+]
+
+console.log = (superman ["forca"] + superman["agilidade"] + superman["coragem"]) / 3
+
+
+let nivel_herois = (" superman " +  90 + 95 + 100 / 3)
+
+
