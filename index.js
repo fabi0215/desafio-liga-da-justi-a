b7b6be70a9e4ac07d7cecb61b7f702ab44477640
fabi0215/ <fabianeranzani@gmail.com>
2024-06-30 01:27:15 -0300

    Inicializar

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 let nomes_herois = [" superman" , " batman " ,  " flash " ];
 console .log ("olá herois " + nomes_herois);
 
-let forca =  [" de 0 - 100"     = 95 , 75 , 60]
-let agilidade = [" de 0 - 100"  = 100 , 95 , 60]
-let coragem = [" de 0 - 100"    = 100 , 100 , 90]
+let valores_das_habilidades
+[forca =  [" valores"     = 95 , 75 , 60]
+[agilidade = [" valores"  = 100 , 95 , 60]
+[coragem = [" valores"    = 100 , 100 , 90]
 
-let habilidades_dos_herois = [
-[" superman " , (" forca " +  95 ) , (" agilidade " +  95 ) , (" coragem " +  100 ) ],
-[" batman" , 60 , 60 , 100 ],
-[" flash " , 75 , 100 , 90 ],
 ]
 
+let habilidades_dos_herois = [
+    [" superman " , (" forca " +  95 ) , (" agilidade " +  95 ) , (" coragem " +  100 ) ],
+    [" batman" , 60 , 60 , 100 ],
+    [" flash " , 75 , 100 , 90 ],
+    ]
+
+// Defina o objeto com as propriedades
 const superman = {
     forca: 95,
     agilidade: 95,
     coragem: 100
   };
   
+  // Calcule a média das propriedades
   const media = (superman.forca + superman.agilidade + superman.coragem) / 3;
+
+// Imprima o resultado no console
   console.log(media) =  96,66
+
+  console.log(" superman " + valores_das_habilidades_dos_herois[1][0] + " possui agilidade " +  timePokemon[1][1] + 
+    " e está no nível " + timePokemon[1][2] )
   
 
 
