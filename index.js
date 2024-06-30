d490f8651257eb30e07be03bbd4db771c1c74d36
fabi0215/ <fabianeranzani@gmail.com>
2024-06-30 02:39:02 -0300

    informações

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let nomes_herois = [" superman" , " batman " ,  " e flash " ];
 console .log ("olá herois " + nomes_herois);
 
+let habilidades_dos_herois = 
+let
 let forca = [ 95 , 75 , 60]
 let agilidade = [ 100 , 95 , 60]
 let coragem = [ 100 , 100 , 90]
 console .log (" hoje vamos classificar suas habilidades " );
 
-let habilidades_dos_herois = [
-  [" superman " , (" forca " +  95 ) , (" agilidade " +  95 ) , (" coragem " +  100 ) ],
-  [" batman" , 60 , 60 , 100 ],
-  [" flash " , 75 , 100 , 90 ],
+console .log (" superman possui " + " força " , forca [0]+  " agilidade " , agilidade [1]+  " e coragem " , coragem  [0])
+console .log (" bataman possui " + " força " , forca [2]+  " agilidade " , agilidade [2]+  " e coragem " , coragem  [1])
+console .log (" flash possui " + " força " , forca [1]+  " agilidade " , agilidade [0]+  " e coragem " , coragem  [2])
 
-  ]
 
 // Defina o objeto com as propriedades
 const superman = {
@@ -20,19 +20,16 @@ const superman = {
     coragem: 100
   };
   
-  console .log (" superman possui " + "força" , forca + "agilidade " , agilidade + "coragem " , coragem )
+  // Calcule a média das propriedades 
+  let classificando_o_nivel_do_heroi
+  const  media = (superman.forca + superman.agilidade + superman.coragem) / 3;
 
-  // Calcule a média das propriedades
-  const nivel_heroi = (heroi_forca + heroi_agilidade + heroi_coragem) / 3
- 
-  const media = (superman.forca + superman.agilidade + superman.coragem) / 3;
+  let media: [ 96,66 ]
 
-// Imprima o resultado no console
   console.log(media) =  96,66
   nivel_heroi = alto 
 
-  console.log(" superman " + habilidades_dos_herois[1][0] + " possui agilidade " +  agilidade[1][1] + 
-    " e coragem " + coragem[1][2]  + " e seu nivel é alto")
+  console.log(" superman sua classificação é alto" , media , " nivel alto ")
   
 
 
