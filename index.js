f8173a3ab2f35539764c3e9182784e3cdff1be08
fabi0215/ <fabianeranzani@gmail.com>
2024-06-30 03:10:32 -0300

    desafio feito

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
-let nomes_herois = [" superman" , " batman " ,  " e flash " ];
-console .log ("olá herois " + nomes_herois);
+let nomes_herois = ["superman", "batman", "flash"];
+console.log("Olá heróis " + nomes_herois.join(", ") + "!");
 
-let habilidades_dos_herois = 
-let
-let forca = [ 95 , 75 , 60]
-let agilidade = [ 100 , 95 , 60]
-let coragem = [ 100 , 100 , 90]
-console .log (" hoje vamos classificar suas habilidades " );
+let forca = [95, 75, 60];
+let agilidade = [100, 95, 60];
+let coragem = [100, 100, 90];
 
-console .log (" superman possui " + " força " , forca [0]+  " agilidade " , agilidade [1]+  " e coragem " , coragem  [0])
-console .log (" bataman possui " + " força " , forca [2]+  " agilidade " , agilidade [2]+  " e coragem " , coragem  [1])
-console .log (" flash possui " + " força " , forca [1]+  " agilidade " , agilidade [0]+  " e coragem " , coragem  [2])
+console.log("Hoje vamos classificar suas habilidades.");
+
+console.log("Superman possui força " + forca[0] + ", agilidade " + agilidade[0] + " e coragem " + coragem[0]);
+console.log("Batman possui força " + forca[1] + ", agilidade " + agilidade[1] + " e coragem " + coragem[1]);
+console.log("Flash possui força " + forca[2] + ", agilidade " + agilidade[2] + " e coragem " + coragem[2]);
 
 
-// Defina o objeto com as propriedades
+
+// Defina os objetos com as propriedades
 const superman = {
-    forca: 95,
-    agilidade: 95,
-    coragem: 100
-  };
-  
-  // Calcule a média das propriedades 
-  let classificando_o_nivel_do_heroi
-  const  media = (superman.forca + superman.agilidade + superman.coragem) / 3;
+  nome: 'Superman',
+  forca: 95,
+  agilidade: 95,
+  coragem: 100
+};
 
-  let media: [ 96,66 ]
+const batman = {
+  nome: 'Batman',
+  forca: 60,
+  agilidade: 60,
+  coragem: 100
+};
 
-  console.log(media) =  96,66
-  nivel_heroi = alto 
+const flash = {
+  nome: 'Flash',
+  forca: 75,
+  agilidade: 100,
+  coragem: 90
+};
 
-  console.log(" superman sua classificação é alto" , media , " nivel alto ")
-  
+// Função para calcular a média das habilidades
+function calcularMedia(heroi) {
+  return ((heroi.forca + heroi.agilidade + heroi.coragem) / 3).toFixed(2);
+}
 
+// Função para determinar o nível do herói
+function determinarNivel(media) {
+  if (media >= 90) {
+    return "alto";
+  } else if (media >= 70) {
+    return "médio";
+  } else {
+    return "baixo";
+  }
+}
 
+// Função para exibir a classificação do herói
+function exibirClassificacao(heroi) {
+  const media = calcularMedia(heroi);
+  const nivel = determinarNivel(media);
+  console.log(`${heroi.nome} sua classificação é ${nivel} com uma média de ${media}`);
+}
 
-
+// Exibir a classificação para cada herói
+exibirClassificacao(superman);
+exibirClassificacao(batman);
+exibirClassificacao(flash);
